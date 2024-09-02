1389eb15c1b816fc9789e4716ece5100f08d6935
jame25 <jame25@users.noreply.github.com>
2024-09-02 09:47:09 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,17 +1,33 @@
 using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace PiperTrayClassic
 {
     static class Program
     {
+        static Mutex mutex = new Mutex(true, "{8F6F0AC4-B9A1-45fd-A8CF-72F04E6BDE8F}");
+
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new PiperTrayApp());
+            if (mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                try
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new PiperTrayApp());
+                }
+                finally
+                {
+                    mutex.ReleaseMutex();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Another instance of Piper Tray Classic is already running.", "Piper Tray Classic", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
     }
 }
